4064c03d234755f0d2bd64fdd6c17fea7a72908e
github username <github email address>
2021-02-17 18:09:31 -0500

    methods working

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,4 +5,22 @@ class OrdersController < ApplicationController
         render json: orders, :except => [:created_at, :updated_at]
     end 
 
+    def update
+        order = Order.find(params[:id])
+        order.update(orders_params)
+        render json: order
+    end 
+
+    
+    def create
+        order = Order.create!(orders_params)
+        render json: order
+    end 
+
+        private 
+    def orders_params
+        params.require(:order).permit(:user_id, :date, :checked_out)
+        # render json: item_order 
+    end 
+
 end
